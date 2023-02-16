2317d5d3702b1982e1117cd1ce33d1595efa57ba
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2023-02-16 18:55:12 +0200

    Initial Commit

---
+++ Xdebug Companion Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,26 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Xdebug Companion Extension
+//
+//  Created by Ihor Vorotnov on 16.02.2023.
+//
+
+import SafariServices
+import os.log
+
+let SFExtensionMessageKey = "message"
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+	func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+    
+}
